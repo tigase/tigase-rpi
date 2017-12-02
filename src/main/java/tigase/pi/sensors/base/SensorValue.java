ce04d4ec70d1bf1ea334077663b388f7462bcbe5
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2017-12-02 11:20:13 +0100

    #6275: updated licensing headers

--- src/main/java/tigase/pi/sensors/base/SensorValue.java
+++ src/main/java/tigase/pi/sensors/base/SensorValue.java
@@ -1,18 +1,22 @@
 /*
- * Copyright (C) 2016 Artur Hefczyc <artur.hefczyc at tigase.net>
+ * SensorValue.java
+ *
+ * Tigase RPi Library
+ * Copyright (C) 2016-2017 "Tigase, Inc." <office@tigase.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
  */
 package tigase.pi.sensors.base;
 
